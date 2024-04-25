6b016c404e5061bc5457d8395bffa79ed1c4cfbe
nicustei <niculin.steiner@ergon.ch>
2024-04-25 10:09:33 +0200

    add some error handling

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,13 +9,17 @@ var addressBookEntryStore: List[AddressBookEntry] = List.empty
 def addEntry(): Unit = {
   addressBookEntryStore = AddressBookEntryRepository().saveNewEntry(addressBookEntryStore, AddressBookEntryFactory().getAddressBookDataFromUser.unsafeRunSync())
   IO.println("Ihre Einträge:\n").unsafeRunSync()
-  addressBookEntryStore.foreach(entry => IO.println(s"Adresse ${addressBookEntryStore.indexOf(entry) + 1}: \n$entry" ).unsafeRunSync())
+  addressBookEntryStore.foreach(entry => IO.println(s"Adresse ${addressBookEntryStore.indexOf(entry) + 1}: \n$entry").unsafeRunSync())
 }
 
 object Main {
   def main(args: Array[String]): Unit = {
-    while(true){
-      addEntry()
+    while (true) {
+      try {
+        addEntry()
+      } catch
+        case e: IllegalArgumentException => IO.println("Eingabe war falsch!: " + e.getMessage).unsafeRunSync()
+
     }
   }
 }
--- src/main/scala/domain/person/Email.scala
+++ src/main/scala/domain/person/Email.scala
@@ -1,5 +1,5 @@
 package domain.person
 
-case class Email(email: String) {
-  require(email.matches("""^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$"""))
+case class Email(email: String){
+  require(email.matches("""^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$"""), "Must match the mail pattern!")
 }
