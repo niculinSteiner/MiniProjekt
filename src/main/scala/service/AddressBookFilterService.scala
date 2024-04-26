ac0f70bd2563fdb2e3eaa40272acfa25bd0b7673
Mikka <mikka.widmer@adnovum.ch>
2024-04-26 16:31:14 +0200

    add FilterTrait.

--- src/main/scala/service/AddressBookFilterService.scala
+++ src/main/scala/service/AddressBookFilterService.scala
@@ -8,6 +8,10 @@ object AddressBookFilterService {
     filterArgument match {
       case _ : Category => entries.filter(_.category == filterArgument)
       case _ : Email => entries.filter(_.mail == filterArgument)
+      case _ : FirstName => entries.filter(_.firstName == filterArgument)
+      case _ : LastName => entries.filter(_.lastName == filterArgument)
+      case _ : PhoneNumber => entries.filter(_.phoneNumber == filterArgument)
+      case _ : Address => entries.filter(_.address == filterArgument)
       case _  => entries //if nothing found
     }
   }
